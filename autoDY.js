02e51e345ca2588419ead365a79a4cd3585d476f
tanmingyang <tanmingyang@yryz.com>
2020-06-12 09:34:13 +0800

    支持多台设备滑动

--- autoDY.js
+++ autoDY.js
@@ -1,10 +1,35 @@
 const child_process = require("child_process");
+const readline = require("readline");
+const stream = require("stream");
 
 var index = 0;
 
 function autoAdb() {
   console.log(`自动滑动第${++index}`);
-  child_process.exec(`adb  shell  input swipe 540 1300 450 500 100`);
+
+  var cmdStr = child_process.execSync(`adb devices`);
+
+  var bufferStream = new stream.PassThrough();
+  bufferStream.end(cmdStr);
+
+  var rl = readline.createInterface({ input: bufferStream });
+
+  var count = 0;
+
+  rl.on("line", (line) => {
+    if (count > 0 && line.indexOf("device") != -1) {
+      var devicesId = line.replace("device", "").trim();
+      console.log(`当前正在唤醒的设备id：${devicesId}`);
+      try {
+        child_process.execSync(
+          `adb -s ${devicesId} shell  input swipe 540 1300 450 500 100`
+        );
+      } catch (error) {
+        console.log(`执行滑动指令失败....`);
+      }
+    }
+    count++;
+  });
 }
 
 async function loop() {
